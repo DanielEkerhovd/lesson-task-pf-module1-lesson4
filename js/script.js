e3f541646e4478a0b3b3ead4c3cb71347b8c0890
Daniel-Ekerhovd <danielekerh@gmail.com>
2023-09-19 14:17:25 +0200

    Finished

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Question 1
+console.log("");
+
+//Zitch dog!
+var dogCount = 0;
+
+for (dogCount; dogCount <= 7; dogCount++) {
+    console.log(dogCount);
+}
+
+
+//Question 2
+console.log("");
+
+var yellowCar = 10;
+
+for (yellowCar; yellowCar >= 0; yellowCar--) {
+    console.log(yellowCar);
+}
+
+
+//Question 3 
+console.log("")
+
+var coins = 10;
+
+for (coins; coins <= 20; coins++) {
+   if ((coins % 2) !== 0) {
+    (console.log(coins)); }
+}
